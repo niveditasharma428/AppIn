31433ef839540b60448ec77cacb7f19a7035fcd0
niveditasharma428 <niveditasharma428@gmail.com>
2017-01-19 01:10:21 +0530

    new

--- app/src/main/java/com/example/appin/appin/MainActivity.java
+++ app/src/main/java/com/example/appin/appin/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.appin.appin;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -13,9 +14,11 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -27,8 +30,9 @@ public class MainActivity extends AppCompatActivity {
     private EditText email,password;
     private Button sign_in_register;
     private RequestQueue requestQueue;
-    private static final String URL = "https://appen.000webhostapp.com/login.php";
-    private StringRequest request;
+    private static final String URL = "http://itinfocube.info/dev1/urgoe/login";
+    private JsonObjectRequest request;
+    String respons;
 
 
     @Override
@@ -39,45 +43,60 @@ public class MainActivity extends AppCompatActivity {
         email = (EditText) findViewById(R.id.email);
         password = (EditText) findViewById(R.id.password);
         sign_in_register = (Button) findViewById(R.id.login);
-
         requestQueue = Volley.newRequestQueue(this);
         sign_in_register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                request = new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
-                        try {
-                            JSONObject jsonObject = new JSONObject(response);
-                            if(jsonObject.names().get(0).equals("success")){
-                                Toast.makeText(getApplicationContext(),"Success"+jsonObject.getString("success"),Toast.LENGTH_SHORT).show();
-                                startActivity(new Intent(getApplicationContext(),Welcome.class));
-                            } else {
-                                Toast.makeText(getApplicationContext(),"Error"+jsonObject.getString("error"),Toast.LENGTH_SHORT).show();
+                final HashMap<String,String> hashMap = new HashMap<String, String>();
+                hashMap.put("username",email.getText().toString());
+                hashMap.put("password",password.getText().toString());
+                final JSONObject param = new JSONObject(hashMap);
+                Log.d("Nivi",param.toString());
+                        request = new JsonObjectRequest(URL, param,new Response.Listener<JSONObject>() {
+                            @Override
+                            public void onResponse(JSONObject response){
+                                respons = response.toString();
+                                try {
+                                    parsestring();
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        }, new Response.ErrorListener() {
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
 
                             }
-                        } catch (JSONException e) {
-                            e.printStackTrace();
-                        }
-
+                        });
+                        requestQueue.add(request);
                     }
-                }, new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-
-                    }
-                }){
-                    @Override
-                    protected Map<String, String> getParams() throws AuthFailureError {
-                        HashMap<String,String> hashMap = new HashMap<String, String>();
-                        hashMap.put("email",email.getText().toString());
-                        hashMap.put("password",password.getText().toString());
-
-                        return hashMap;
-                    }
-                };
-                requestQueue.add(request);
+                });
             }
-        });
+
+    public void parsestring() throws JSONException
+    {
+        Log.d("Nivi","Called");
+
+        JSONObject jsonObj = new JSONObject(respons);
+        JSONArray ob = jsonObj.getJSONArray("data");
+        JSONObject data = ob.getJSONObject(0);
+        String fname = data.getString("firstname");
+        String lname = data.getString("lastname");
+        String email = data.getString("email");
+
+        Toast.makeText(getApplicationContext(),respons,Toast.LENGTH_LONG).show();
+
+        Intent i = new Intent(getApplicationContext(),Welcome.class);
+        i.putExtra("fname",fname);
+        i.putExtra("lname",lname);
+        i.putExtra("email",email);
+
+        startActivity(i);
+
+
+        Log.d("Nivi",fname);
+        Log.d("Nivi",lname);
+        Log.d("Nivi",email);
     }
 }
+
--- app/src/main/java/com/example/appin/appin/Welcome.java
+++ app/src/main/java/com/example/appin/appin/Welcome.java
@@ -3,8 +3,12 @@ package com.example.appin.appin;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class Welcome extends AppCompatActivity {
 
@@ -14,6 +18,22 @@ public class Welcome extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+        TextView fname = (TextView) findViewById(R.id.fname);
+        TextView lname = (TextView) findViewById(R.id.lname);
+        TextView email = (TextView) findViewById(R.id.email);
+
+        Intent i = getIntent();
+        String fnameS = i.getStringExtra("fname");
+        String lnameS = i.getStringExtra("lname");
+        String Semail = i.getStringExtra("email");
+
+       /* Log.d("Nivi",fnameS);
+        Log.d("Nivi",lnameS);
+        Log.d("Nivi",Semail);
+*/
+        fname.setText(fnameS);
+        lname.setText(lnameS);
+        email.setText(Semail);
 
         logout = (Button) findViewById(R.id.logout);
         logout.setOnClickListener(new View.OnClickListener() {
